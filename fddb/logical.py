a2b477e244ad0a012df0d6d18399c43d8d0f2b37
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-09-09 22:53:24 +0600

    logical implementation of FBDB

---
+++ fddb/logical.py
@@ -0,0 +1,18 @@
+class LogicalBase(object):
+
+    def get(self, key):
+        if not self._storage.locked:
+            self._refresh_tree_ref()
+        return self._get(self._follow(self._tree_ref), key)
+
+    def set(self, key,value):
+        if self._storage.lock():
+            self._refresh_tree_ref()
+        self._tree_ref = self._insert(
+            self._follow(self._tree_ref), key, self.value_ref_calss(value)
+        )
+
+    def _refresh_tree_ref(self):
+        self._tree_ref = self.node_ref_class(
+            address=self._storage.get_root_address()
+        )
