8314766e8193f9d9935a042f3731835b855327b6
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-09-09 22:52:15 +0600

    connection implementation for DB

---
+++ fddb/__init__.py
@@ -0,0 +1,11 @@
+import os
+from .interface import FBDB
+
+
+def connect(dbname):
+    try:
+        f = open(dbname, 'r+b')
+    except IOError:
+        fd = os.open(dbname, os.O_RDWR | os.O_CREAT)
+        f = os.fdopen(fd, 'r+b')
+    return FBDB(f)
