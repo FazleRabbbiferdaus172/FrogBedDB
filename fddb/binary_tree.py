404c560151daa6f15457fca419dff45f3ce80ed7
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-09-09 22:54:06 +0600

    partial binary tree implementation of FBDB. only get method is implementaed

---
+++ fddb/binary_tree.py
@@ -0,0 +1,12 @@
+from .logical import LogicalBase
+
+class BinaryTree(LogicalBase):
+    def _get(self, node, key):
+        while node is not None:
+            if key < node.key:
+                node = self._follow(node.left_ref)
+            elif node.key < key:
+                node = self._follow(node.right_ref)
+            else:
+                return self._follow(node.value_ref)
+        raise KeyError
