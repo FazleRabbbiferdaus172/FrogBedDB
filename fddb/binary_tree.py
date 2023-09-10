165ba4deaa57b1c16061d05c48c330a4d22abffe
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-09-11 00:03:40 +0600

    store_refs and referent_to_string, prepare_to_store and _insert implemented

--- fddb/binary_tree.py
+++ fddb/binary_tree.py
@@ -2,11 +2,27 @@ from .logical import LogicalBase, ValueRef
 
 
 class BinaryNode:
-    pass
+    def store_refs(self, storage):
+        self.value_ref.store(storage)
+        self.left_ref.store(storage)
+        self.right_ref.store(storage)
 
 
 class BinaryNodeRef(ValueRef):
-    pass
+
+    @staticmethod
+    def referent_to_string(referent):
+        return pickle.dumps({
+            'left': referent.left_ref.address,
+            'key': referent.key,
+            'value': referent.value_ref.address,
+            'right': referent.right_ref.address,
+            'length': referent.length,
+        })
+
+    def prepare_to_store(self, storage):
+        if self._referent:
+            self._referent.store_refs(storage)
 
 
 class BinaryTree(LogicalBase):
@@ -19,3 +35,17 @@ class BinaryTree(LogicalBase):
             else:
                 return self._follow(node.value_ref)
         raise KeyError
+
+    def _insert(self, node, key, value_ref):
+        if node is None:
+            new_node = BinaryNode(self.node_ref_class(), key, value_ref, self.node_ref_calss(), 1)
+        elif key < node.key:
+            new_node = BinaryNode.from_node(
+                node,
+                left_ref=self._insert(
+                    self.follow(node.left_ref), key, value_ref
+                )
+            )
+        else:
+            new_node = BinaryNode.from_node(node, value_ref=value_ref)
+        return self.node_ref_class(referent=new_node)
