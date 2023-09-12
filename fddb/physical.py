25b28b3a4a0b0629240c10522ee7928cf5da5827
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-09-13 01:20:41 +0600

    _ensure_superblock implemented and first test case of test_physical test_init_ensures_superblock passes

--- fddb/physical.py
+++ fddb/physical.py
@@ -1,4 +1,30 @@
+import os
+import portalocker
+
 class Storage:
+    SUPERBLOCK_SIZE = 4096
+
+    def __init__(self, f):
+        self._f = f
+        self.locked = False
+        self._ensure_superblock()
+
+    def _seek_end(self):
+        self._f.seek(0, os.SEEK_END)
+    def _ensure_superblock(self):
+        self.lock()
+        self._seek_end()
+        end_address = self._f.tell()
+        if end_address < self.SUPERBLOCK_SIZE:
+            self._f.write(b'\x00' * (self.SUPERBLOCK_SIZE - end_address))
+        self.unlock()
+
+    def unlock(self):
+        if self.locked:
+            self._f.flush()
+            portalocker.unlock(self._f)
+            self.locked = False
+
     def lock(self):
         if not self.locked:
             portalocker.lock(self._f, portalocker.LOCK_EX)
